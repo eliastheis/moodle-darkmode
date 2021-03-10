fb8b9992ee7dd64eb1bc660f5388607a21f12ca7
eliastheis <eliastheis.sg@gmail.com>
2021-03-10 16:52:00 +0100

    Added darkmode for /mod/forum

---
+++ sites/mod_forum.js
@@ -0,0 +1,21 @@
+console.log('[DarkMode] loading mod_forum.js...');
+
+elems = document.getElementsByClassName('region_main_settings_menu_proxy');
+for (let i = 0; i < elems.length; i++) {
+	elems[i].style.backgroundColor = backgroundColor;
+}
+
+elems = document.getElementsByClassName('author-info align-middle');
+for (let i = 0; i < elems.length; i++) {
+	elems[i].style.color = textColor;
+}
+
+elems = document.getElementsByClassName('text-center');
+for (let i = 0; i < elems.length; i++) {
+	elems[i].style.color = textColor;
+}
+
+elems = document.getElementsByClassName('icon fa fa-ellipsis-v fa-fw');
+for (let i = 0; i < elems.length; i++) {
+	elems[i].style.color = textColor;
+}
