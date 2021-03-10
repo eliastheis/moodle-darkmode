d1640e5e5ad2e1343803ef57bb23851e2de6df0d
eliastheis <eliastheis.sg@gmail.com>
2021-03-10 17:44:32 +0100

    Added darkmode for policies

---
+++ sites/admin_tool_policy_viewall.js
@@ -0,0 +1,11 @@
+console.log('[DarkMode] loading admin_tool_policy_viewall.js...');
+
+elems = document.querySelectorAll('[scope="col"]');
+for (let i = 0; i < elems.length; i++) {
+	elems[i].style.color = textColor;
+}
+
+elems = document.getElementsByTagName('td');
+for (let i = 0; i < elems.length; i++) {
+	elems[i].style.color = textColor;
+}
