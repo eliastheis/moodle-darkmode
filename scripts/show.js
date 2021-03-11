eeb6ef9a356e7de018c1bb70d6ce0c41ad03c068
eliastheis <eliastheis.sg@gmail.com>
2021-03-11 11:05:24 +0100

    Fixed anit-pattern JS-0239
    Require let or const instead of var

--- scripts/show.js
+++ scripts/show.js
@@ -1,5 +1,5 @@
 document.body.style.setProperty('display', 'inherit', 'important');
 
-let diff = new Date().getMilliseconds() - start_date.getMilliseconds()
+const diff = new Date().getMilliseconds() - start_date.getMilliseconds()
 
 console.log('[DarkMode] finished in ' + diff + 'ms');
